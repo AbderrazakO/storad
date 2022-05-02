5390cb431c46613db40bea8f81c3833bd6f65730
Ousssssssssi <abderrazaqoussimbarek@gmail.com>
2022-05-02 17:24:13 +0000

    AID 2.0

--- src/Components/Shop/Filter/DragFilter/Index.js
+++ src/Components/Shop/Filter/DragFilter/Index.js
@@ -1,87 +1,59 @@
 import { useEffect, useRef, useState } from 'react'
 
-const Index = () => {
+const Index = ({ minValue = 0, maxValue = 1000 }) => {
   const leftPoint = useRef(null)
-  const ligne = useRef(null)
+  const progress = useRef(null)
   const rightPoint = useRef(null)
-  const [leftDown, setLeftDown] = useState(false)
-  const startX = useRef(0) // Get Mouse Down Position
-  const endX = useRef(0) // Get Mouse On Mouve Position
 
-  // Set Ligne Position
+  //
   useEffect(() => {
-    const startPoint = leftPoint.current.offsetLeft
-    ligne.current.style.left = `${startPoint}px`
-    const endPoint = rightPoint.current.offsetLeft
-    ligne.current.style.width = `${endPoint}px`
-  })
+    const minValueP = (leftPoint.current.value / leftPoint.current.max) * 100
+    const maxValueP =
+      100 - (rightPoint.current.value / rightPoint.current.max) * 100
 
-  // Set Mouse Events
-  const mousedown = (event) => {
-    startX.current = event.pageX - leftPoint.current.offsetLeft // Get Mouse Initial Position
-    setLeftDown(true)
-  }
-
-  const mouseup = (event) => {
-    setLeftDown(false)
-  }
-
-  const mousemove = (event) => {
-    event.preventDefault()
-
-    endX.current = event.pageX // Set Cursor Position
-    const endPoint = rightPoint.current.offsetLeft
-    const startPoint = leftPoint.current.offsetLeft
-    //
-    if (!leftDown || startPoint >= endPoint || startPoint < 0) return
-    leftPoint.current.style.left = `${endX.current - startX.current}px` // Move Slider
-    ligne.current.style.left = `${startPoint}px`
-    // }
-  }
-
-  // Add Mouse Events
-  useEffect(() => {
-    //
-    leftPoint.current.addEventListener('mousedown', mousedown)
-    leftPoint.current.addEventListener('mousemove', mousemove)
-    leftPoint.current.addEventListener('mouseup', mouseup)
-
-    //
-    // rightPoint.current.addEventListener('mousedown', mousedown)
-    // rightPoint.current.addEventListener('mousemove', mousemove)
-    // rightPoint.current.addEventListener('mouseup', mouseup)
-    // rightPoint.current.addEventListener('mouseleave', mouseleave)
-
-    return () => {
-      //
-      leftPoint.current.removeEventListener('mousedown', mousedown)
-      leftPoint.current.removeEventListener('mousemove', mousemove)
-      leftPoint.current.removeEventListener('mouseup', mouseup)
-
-      //
-      // rightPoint.current.removeEventListener('mousedown', mousedown)
-      // rightPoint.current.removeEventListener('mousemove', mousemove)
-      // rightPoint.current.removeEventListener('mouseup', mouseup)
-      // rightPoint.current.removeEventListener('mouseleave', mouseleave)
-    }
+    progress.current.style.left = `${minValueP}%`
+    progress.current.style.right = `${maxValueP}%`
   })
 
   //
+  const setProgress = () => {
+    const minValueP = (leftPoint.current.value / leftPoint.current.max) * 100
+    const maxValueP =
+      100 - (rightPoint.current.value / rightPoint.current.max) * 100
+
+    progress.current.style.left = `${minValueP}%`
+    progress.current.style.right = `${maxValueP}%`
+  }
+
   return (
     <div className='DragableFilter'>
       <div className='dragFilterTitle'>{`Price`}</div>
       <div className='valueBox'>
         <div className='cardValue'>
-          From <span>250</span> $
+          From <input type='text' name='' id='' /> $
         </div>
         <div className='cardValue'>
-          Up To <span>3490</span> $
+          Up To <input type='text' name='' id='' /> $
         </div>
       </div>
       <div className='dragAria'>
-        <div ref={leftPoint} className='leftPoint'></div>
-        <div ref={ligne} className='ligne'></div>
-        <div ref={rightPoint} className='rightPoint'></div>
+        <input
+          ref={leftPoint}
+          className='leftPoint'
+          type='range'
+          min={minValue}
+          max={maxValue}
+          onChange={setProgress}
+        />
+        <div ref={progress} className='progress'></div>
+        <input
+          ref={rightPoint}
+          className='rightPoint'
+          type='range'
+          min={minValue}
+          max={maxValue}
+          onChange={setProgress}
+        />
       </div>
     </div>
   )
