568d39d69dddfc6dbc078cc34b871ae90179b6f8
Ousssssssssi <abderrazaqoussimbarek@gmail.com>
2022-05-07 12:54:08 +0000

    Footer Done

--- src/Layouts/Footer/Index.js
+++ src/Layouts/Footer/Index.js
@@ -10,42 +10,40 @@ const Index = () => {
         <div className='footerCol'>
           <Logo />
           <div className='brandDescription'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            natus illum soluta iure, earum voluptate at molestias iusto. Nam,
-            beatae?
+            {`The Shoe Company is a Moroccan shoe store, originating in Greater Rabat Area in 1992. From its inception, The Shoe Company was operated by conglomerate Town Shoes. The founder of Town Shoes, Leonard Simpson, had predicted a growth opportunity for footwear to be sold in a big box format.`}
           </div>
         </div>
         <div className='footerCol'>
           <h1 className='footerTitle'>Shop</h1>
-          <a href='' className='footerLink'>
-            Nuts
+          <a href='/shop/sneakers' className='footerLink'>
+            Sneakers
           </a>
-          <a href='' className='footerLink'>
-            Leaf
+          <a href='/shop/sandals' className='footerLink'>
+            Sandals
           </a>
-          <a href='' className='footerLink'>
-            Lorem0
+          <a href='/shop/loafers' className='footerLink'>
+            Loafers
           </a>
-          <a href='' className='footerLink'>
-            Lorem1
+          <a href='/shop/boots' className='footerLink'>
+            Boots
           </a>
-          <a href='' className='footerLink'>
-            Lorem3
+          <a href='/shop/heels' className='footerLink'>
+            Heels
           </a>
         </div>
         <div className='footerCol'>
           <h1 className='footerTitle'>Social</h1>
           <Media />
-          <a href='' className='footerLink'>
+          <a href='/' className='footerLink'>
             Facebook
           </a>
-          <a href='' className='footerLink'>
+          <a href='/' className='footerLink'>
             Whatsapp
           </a>
-          <a href='' className='footerLink'>
+          <a href='/' className='footerLink'>
             Instagram
           </a>
-          <a href='' className='footerLink'>
+          <a href='/' className='footerLink'>
             Discord
           </a>
         </div>
